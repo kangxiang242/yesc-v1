067d3d951125e2de6e0894c9ed8f0b8302c17912
HK-Company-Raingor <raingor00@gmail.com>
2026-08-10 10:15:02 +0800

    Revert "fix: 资源 navigationGroup 分组补齐（文章/訂單/商品/SEO）"
    This reverts commit 28612098ed4f3458a228604d1df6e25da20d20d2.

--- app/Filament/Resources/ArticleResource.php
+++ app/Filament/Resources/ArticleResource.php
@@ -18,8 +18,6 @@ class ArticleResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
-    protected static ?string $navigationGroup = '內容管理';
-
     protected static ?string $navigationLabel = '文章管理';
 
     protected static ?string $label = '文章';
